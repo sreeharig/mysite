ecd6bebe665fabd5fddb4b45a7285e5907ef01ff
sreeharig <sgbhattathiri@gmail.com>
2016-06-02 17:07:01 +0530

    webpage

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from registration.views import Home
-from django.conf.urls import include
-
+from registration import urls as reg_url
+from django.conf.urls import include, url
 urlpatterns = [
-    url(r'^$', Home.as_view(), name='home'),
-    url(r'^admin/', include(admin.site.urls))
-
+        url(r'^$', Home.as_view(), name='home'),
+        url(r'^admin/', include(admin.site.urls)),
+        url(r'^registration/', include(reg_url)),
 
 ]
 
---
+++ registration/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include, url
+from registration.views import *
+from registration.models import *
+
+
+urlpatterns = [
+    url(r'^$', UserRegistrationView.as_view(), name='register_user'),
+]
+

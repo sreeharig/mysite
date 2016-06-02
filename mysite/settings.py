1f9d457d7b7ceb55365503fb3139f0828d5e11e7
sreeharig <sgbhattathiri@gmail.com>
2016-06-02 10:15:37 +0530

    wp

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,8 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = '/media/'STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (MEDIA_ROOT,)
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = '/static/'
+

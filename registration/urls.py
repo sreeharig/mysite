bd403ce9e6e896fd9200f9da455ff592d9aeedd1
sreeharig <sgbhattathiri@gmail.com>
2016-06-04 12:20:04 +0530

    webpagesuccess

--- registration/urls.py
+++ registration/urls.py
@@ -6,4 +6,6 @@ from registration.models import *
 urlpatterns = [
     url(r'^$', UserRegistrationView.as_view(), name='register_user'),
 
-]
+    url(r'^user/success/', TemplateView.as_view(template_name='success.html'),
+    name='page')
+    ]

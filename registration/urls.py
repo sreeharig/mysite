35260d2ceb3d8b729f61fa476461d6b9289d181f
sreeharig <sgbhattathiri@gmail.com>
2016-06-07 12:33:34 +0530

    list choco

--- registration/urls.py
+++ registration/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
 
     url(r'^user/success/', TemplateView.as_view(template_name='success.html'),
     name='page'),
-    url(r'^chocolate/add/', AddChocolateView.as_view(), name='add_chocolates')
-    ]
+    url(r'^chocolate/add/', AddChocolateView.as_view(), name='add_chocolates'),
+    url( r'^chocolate/info/(?P<choco_id>\d+)/$', ChocolateDetailsView.as_view(), name="chocolate_info"),
+    url(r'^user/profile/edit/$', UserProfileUpdateView.as_view(), name='user_profile_update'),
+]
